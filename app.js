fa1b3385220de87e137edc71ce6dc377bf26d7ac
JSNavey <jsnavey@gmail.com>
2018-12-02 23:21:24 -0500

    Set variables Cache Dom Elements

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("TEST!");
+const userScore = 0;
+const computerScore = 0;
+
+// Store DOM Elements as variables (cache DOM Elements)
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
