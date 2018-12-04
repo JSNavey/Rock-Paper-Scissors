bac412305470448b1672a86ddddd172f16613f57
JSNavey <jsnavey@gmail.com>
2018-12-03 22:52:56 -0500

    Add main function and game function for user choice

--- app.js
+++ app.js
@@ -10,15 +10,28 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-// Add action to each rock paper sciossors DOM Element
-rock_div.addEventListener('click', function() {
-  console.log("Rock");
-});
+// userChoice passes rock/paper/sciossors from addEventListening as argument
+function game(userChoice) {
+  console.log("I choose " + userChoice);
+}
 
-paper_div.addEventListener('click', function() {
-  console.log("Paper");
-});
+// Add click addEventListener to each rock paper sciossors DOM Element
+// Then wraping them in main function
+function main() {
+  rock_div.addEventListener('click', function() {
+    // console.log("Rock");
+    game("rock!")
+  });
+  
+  paper_div.addEventListener('click', function() {
+    // console.log("Paper");
+    game("paper!")
+  });
+  
+  scissors_div.addEventListener('click', function() {
+    // console.log("Scissors");
+    game("scissors!")
+  });
+}
 
-scissors_div.addEventListener('click', function() {
-  console.log("Scissors");
-});
+main();
